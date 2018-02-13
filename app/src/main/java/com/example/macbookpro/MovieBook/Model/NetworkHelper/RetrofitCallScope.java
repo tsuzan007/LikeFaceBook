6e1b5c2b3d3a1f5e90ebe7eb8b1fd488210c4005
tsuzan007 <tsuzanprajapati@gmail.com>
2018-02-13 17:20:13 -0500

    An annotation file to make Retrofit client provide a same instance every time.

---
+++ app/src/main/java/com/example/macbookpro/MovieBook/Model/NetworkHelper/RetrofitCallScope.java
@@ -0,0 +1,14 @@
+package com.example.macbookpro.MovieBook.Model.NetworkHelper;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Scope;
+
+/**
+ * Created by macbookpro on 2/13/18.
+ */
+@Scope
+@Retention(RetentionPolicy.CLASS)
+public @interface RetrofitCallScope {
+}
